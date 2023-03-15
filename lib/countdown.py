bc676a3ca6b7dc4fe2504149f26bea14ce433f5d
Beau Kim <beaukim3@gmail.com>
2023-03-15 15:38:22 -0400

    finished

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+import time
+
+def countdown(num):
+
+    while num:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+
+    print('HAPPY NEW YEAR!')
+
+    
+def countdown_with_sleep(num):
+
+    while num:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+        time.sleep(1)
+
+    print('HAPPY NEW YEAR!')
